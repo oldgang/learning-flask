fbb64f10ef348b08e79d204a53c89c37f757be65
oldgang <iceman16999@gmail.com>
2023-09-20 12:06:40 +0200

    Added user authentication check for default page ('/')

--- project/main.py
+++ project/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 from . import db
 
@@ -6,7 +6,9 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    if current_user.is_authenticated:
+        return render_template('index.html')
+    return redirect(url_for('auth.login'))
 
 @main.route('/profile')
 @login_required
